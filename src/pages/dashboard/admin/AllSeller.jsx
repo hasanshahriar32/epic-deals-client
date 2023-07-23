38b4ee92f14e36c6efc603b88503e79c52a6df68
Tofayel <mdtofayelhossain69@gmail.com>
2023-07-23 17:45:52 +0600

    admin action like delete and user verify done

--- src/pages/dashboard/admin/AllSeller.jsx
+++ src/pages/dashboard/admin/AllSeller.jsx
@@ -1,8 +1,11 @@
-;
+
 import { useQuery } from "react-query";
 import BigSpinner from "../../../components/BigSpinner";
 
 import { AiOutlineDelete } from 'react-icons/ai';
+import { MdOutlineVerifiedUser } from 'react-icons/md';
+import { MdVerifiedUser } from 'react-icons/md';
+import { toast } from "react-hot-toast";
 
 
 const AllSeller = () => {
@@ -22,10 +25,53 @@ const AllSeller = () => {
     console.log(allSeller);
 
 
-    // this product will add to advertise 
-    const verifySeller = () =>{
-        alert('coming soon')
+    // verify this seller with this function
+    const verifySeller = (id) =>{
+
+        // const { data,isLoading} = useQuery({
+        //         queryKey:[sellerURL],
+        //         queryFn: async()=>{
+        //             const res = await fetch(`http://localhost:5000/verifyUser?id=${id}`,{
+        //                 method:'PUT',
+        //                 headers: {"Content-Type": "application/json"},
+        //                     })
+
+        //             const result = res.json()
+        //             return result
+        //         }
+        // })
+        
+            fetch(`http://localhost:5000/verifyUser?id=${id}`,{
+                method:'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                  },
+            })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                toast.success('verifyed')
+            })
     }
+
+
+    const handleDeleteUser = (id) => {
+        
+        const confirm = window.confirm('Are U sure to Delete This seller?')
+        
+        if(confirm){
+            fetch(`http://localhost:5000/user?id=${id}`,{
+                method:'DELETE',
+            })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                toast.success('Deleted user')
+            })
+        }
+    }
+
+
 
 
 
@@ -72,7 +118,10 @@ const AllSeller = () => {
                                             </div>
                                             </div>
                                             
-                                            <div className="font-bold">{seller.userName}</div>
+                                            <div className="font-bold" title="verifyed seller">{seller.userName}</div>
+                                            {
+                                                seller.verify && <MdVerifiedUser className='text-green-600'></MdVerifiedUser>
+                                            }
                                             
                                         </div>
                                         </td>
@@ -83,10 +132,12 @@ const AllSeller = () => {
 
                                        
                                         <td>
-                                            <button className='btn btn-xs text-red-500 text-xl'><AiOutlineDelete/></button> 
-                                            <button onClick={()=>verifySeller()} className='btn btn-xs text-blue-500'>
-                                                verify user
-                                            </button>
+                                            <button title="Delete" onClick={()=>handleDeleteUser(seller._id)} className='btn btn-xs text-red-500 text-xl'><AiOutlineDelete/></button> 
+
+                                            {
+                                                !seller.verify && <button title="verify seller" onClick={()=>verifySeller(seller._id)}  className='btn btn-xs  text-blue-500'>add to verify</button>
+                                            }
+                                            
                                         </td>
                                     </tr>
                                     )
